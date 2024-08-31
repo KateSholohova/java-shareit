a56cbd7900d4b40fe597a81d30af612c4cce7789
User Sholokhova <ekaterinasolohova29@gmail.com>
2024-08-31 18:13:33 +0300

    спринт 15

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,17 +1,38 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.user.User;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "bookings")
 @Data
+@NoArgsConstructor
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private Date start;
-    private Date end;
+    @NonNull
+    @Future
+    @Column(name = "start_date")
+    private LocalDateTime start;
+    @NonNull
+    @Future
+    @Column(name = "end_date")
+    private LocalDateTime end;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item;
+    @ManyToOne
+    @JoinColumn(name = "booker_id")
     private User booker;
+    @Enumerated(EnumType.ORDINAL)
     private Status status;
+
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,10 +1,16 @@
 package ru.practicum.shareit.user;
 
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     @Email
